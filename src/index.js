c0dbf21e8b853372656cf1ea6ab14364dd39409d
Eugal-git <eugeniatunha06@gmail.com>
2024-10-18 11:36:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function generateRecipes(event) {
   event.preventDefault();
 
   new Typewriter("#recipes", {
-    strings: "La tombe dit à la rose",
+    strings: "18 Baked Pasta Recipes You Can Make in Your 9x13 Dish",
     autoStart: true,
     delay: 1,
     cursor: "",
