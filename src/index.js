374393f823322a25872914b46f886dbd1388b4ba
Eugal-git <eugeniatunha06@gmail.com>
2024-10-18 11:53:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function generateRecipes(event) {
   event.preventDefault();
 
   new Typewriter("#recipes", {
-    strings: "18 Baked Pasta Recipes You Can Make in Your 9x13 Dish",
+    strings: "pizza recipes",
     autoStart: true,
     delay: 1,
     cursor: "",
