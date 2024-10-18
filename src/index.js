ba373e7a376760d4d6adc64947b1b5e34578cb38
Eugal-git <eugeniatunha06@gmail.com>
2024-10-18 11:34:05 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipes(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipes", {
+    strings: "La tombe dit à la rose",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipesFormElement = document.querySelector("#recipes-generator-form");
+recipesFormElement.addEventListener("submit", generateRecipes);
